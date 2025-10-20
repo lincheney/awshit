db3c4bbb9dd855cdf7a63b0252e1fe32d6040d08
林千里 <lincheney@gmail.com>
2025-10-20 15:53:52 +1100

    json stream plugin

---
+++ json_stream/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import awscli.formatter
+import json
+
+class StreamedJSONFormatter(awscli.formatter.StreamedYAMLFormatter):
+    '''
+    Print newline-delimited JSON for each response page
+    '''
+
+    def __init__(self, args):
+        super().__init__(args, yaml_dumper=json)
+
+    def _flush_stream(self, stream):
+        stream.write('\n')
+        return super()._flush_stream(stream)
+
+def awscli_initialize(event_hooks):
+    event_hooks.register('building-command-table.main', hook)
+
+def hook(command_table, session, command_object, **kwargs):
+    command_object._get_cli_data()['options']['output']['choices'].append('json-stream')
+    awscli.formatter.CLI_OUTPUT_FORMATS['json-stream'] = StreamedJSONFormatter
